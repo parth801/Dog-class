8dd701bac990e9d20845e8bdd4cf1934e0bd4090
parth801 <parthnuwal11@gmail.com>
2024-08-08 09:49:20 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+class Dog():
+  no_of_eyes = 2
+  no_of_ears = 2
+  no_of_tails  = 1
+  no_of_legs = 4
+  def bark(self):
+    print("woof")
+
+German_shephard= Dog()
+
+print(German_shephard.no_of_legs)
